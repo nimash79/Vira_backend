a16330ed66967cd5d636ce6beafd6eefe7f41ab2
Nima <nimash7979@gmail.com>
2026-02-01 17:43:33 +0330

    Initial Commit 2

--- src/routes/deviceRoutes.js
+++ src/routes/deviceRoutes.js
@@ -6,7 +6,7 @@ router.get("/download", async (req, res) => {
   const bytes = Buffer.from("JAHESH\n20 14 1", "utf8");
 
   res.setHeader("Content-Type", "application/octet-stream");
-  res.setHeader("Content-Disposition", 'attachment; filename="hello.txt"');
+  res.setHeader("Content-Disposition", 'attachment; filename="jahesh.txt"');
   res.setHeader("Content-Length", bytes.length);
 
   res.status(200).send(bytes);
